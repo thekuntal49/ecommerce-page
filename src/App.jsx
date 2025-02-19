e59c0158c226f30554cccb94668ec75b5d43ff88
Kuntal Kargupta <167575898+thekuntal49@users.noreply.github.com>
2025-02-19 10:47:07 +0530

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </Router>
   );
