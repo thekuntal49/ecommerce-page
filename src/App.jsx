6a72d935abf39f1afdc34f7efdf3c559e041229e
Kuntal Kargupta <167575898+thekuntal49@users.noreply.github.com>
2025-03-18 21:10:49 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { Visitor } from "./components/Visitor";
 
 const App = () => {
   return (
     <Router>
+      <Visitor />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<HomePage />} />
